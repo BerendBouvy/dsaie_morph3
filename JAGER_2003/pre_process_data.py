a8c08fc445059b5aa163c5234acd6d4289ab366f
Wouter Niessen <w.niessen@student.tudelft.nl>
2024-12-20 14:13:47 +0100

    pre_process_data.py

--- JAGER_2003/pre_process_data.py
+++ JAGER_2003/pre_process_data.py
@@ -4,5 +4,7 @@ import argparse
 from functions import *
 
 def main(input_file, output_file, distance_limit, pixel_size):
-    # Load the input images as 2D numpy arrays
-    data = np.load(input_file)
+    # Load the input images as csv as 2D numpy arrays
+    data = np.loadtxt(input_file, delimiter=',')
+
+    
